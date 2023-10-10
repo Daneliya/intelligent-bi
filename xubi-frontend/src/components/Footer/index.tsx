9a6d20fee46b18f86a7742d1be5330ce034932cc
xu_xiaolong <luckily126163@126.com>
2023-10-10 17:30:05 +0800

    代码提交

--- xubi-frontend/src/components/Footer/index.tsx
+++ xubi-frontend/src/components/Footer/index.tsx
@@ -1,9 +1,10 @@
-import { GithubOutlined } from '@ant-design/icons';
-import { DefaultFooter } from '@ant-design/pro-components';
+import {GithubOutlined} from '@ant-design/icons';
+import {DefaultFooter} from '@ant-design/pro-components';
 import '@umijs/max';
 import React from 'react';
+
 const Footer: React.FC = () => {
-  const defaultMessage = '编程导航知识星球出品';
+  const defaultMessage = '知识星球个人学习项目';
   const currentYear = new Date().getFullYear();
   return (
     <DefaultFooter
@@ -13,23 +14,23 @@ const Footer: React.FC = () => {
       copyright={`${currentYear} ${defaultMessage}`}
       links={[
         {
-          key: '鱼智能 BI',
-          title: '鱼智能 BI',
-          href: 'https://pro.ant.design',
+          key: '个人主页',
+          title: '个人主页',
+          href: 'http://Daneliya.github.io',
           blankTarget: true,
         },
         {
           key: 'github',
-          title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
+          title: <GithubOutlined title='项目源码'/>,
+          href: 'https://github.com/Daneliya/xubi',
           blankTarget: true,
         },
-        {
-          key: '鱼智能 BI',
-          title: '鱼智能 BI',
-          href: 'https://ant.design',
-          blankTarget: true,
-        },
+        // {
+        //   key: '鱼智能 BI',
+        //   title: '鱼智能 BI',
+        //   href: 'https://ant.design',
+        //   blankTarget: true,
+        // },
       ]}
     />
   );
